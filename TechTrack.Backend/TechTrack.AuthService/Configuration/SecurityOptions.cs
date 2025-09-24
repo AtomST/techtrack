613d3b968c97e036262e0197519a009c68bde4b5
Закарян Станислав <stas.zakaryan@gmail.com>
2025-09-24 11:24:07 +0300

    feat(AuthService.Logic): add Access/Refresh token generation logic

---
+++ TechTrack.Backend/TechTrack.AuthService/Configuration/SecurityOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace TechTrack.AuthService.Configuration
+{
+    public class SecurityOptions
+    {
+        public string JwtAccessTokenKey { get; set; } = "";
+        public int JwtAccessTokenDurationInMinutes { get; set; }
+        public int JwtRefreshTokenDurationInDays { get; set; }
+        public string AuthHttpOnlyCookieName { get; set; } = "";
+        public int BCryptWorkFactor { get; set; }
+    }
+}
---
+++ TechTrack.Backend/TechTrack.AuthService/Logic/Interfaces/IJwtLogic.cs
@@ -0,0 +1,8 @@
+﻿namespace TechTrack.AuthService.Logic.Interfaces
+{
+    interface IJwtLogic
+    {
+        public string GenerateAccessToken(Guid userId);
+        public string GenerateRefreshToken();
+    }
+}
