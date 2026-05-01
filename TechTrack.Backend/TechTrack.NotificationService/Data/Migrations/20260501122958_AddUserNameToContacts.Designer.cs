2e63190d4d3c2a62936eae32062f1c29cb4500de
Закарян Станислав <stas.zakaryan@gmail.com>
2026-05-01 15:30:59 +0300

    chore(NotificationService): add UserName to contacts entity

--- TechTrack.Backend/TechTrack.NotificationService/Data/Migrations/20260501122958_AddUserNameToContacts.Designer.cs
+++ TechTrack.Backend/TechTrack.NotificationService/Data/Migrations/20260501122958_AddUserNameToContacts.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using TechTrack.NotificationService.Data;
@@ -11,9 +12,11 @@ using TechTrack.NotificationService.Data;
 namespace TechTrack.NotificationService.Data.Migrations
 {
     [DbContext(typeof(NotificationServiceDbContext))]
-    partial class NotificationServiceDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260501122958_AddUserNameToContacts")]
+    partial class AddUserNameToContacts
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- TechTrack.Backend/TechTrack.NotificationService/UserInfo/Entities/UserContact.cs
+++ TechTrack.Backend/TechTrack.NotificationService/UserInfo/Entities/UserContact.cs
@@ -6,6 +6,7 @@ namespace TechTrack.NotificationService.Projections.Entities
     {
         [Key]
         public Guid UserId { get; set; }
+        public string Name { get; set; }
         public string Email { get; set; }
     }
 }
